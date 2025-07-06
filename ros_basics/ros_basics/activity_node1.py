3cc4de4801cc6671ef2cf656c3242edc1b45b35a
Satyabrata Pradhan <spradhan143as@gmail.com>
2025-07-06 17:07:09 +0200

    Launch files added with paramter related code

--- ros_basics/ros_basics/activity_node1.py
+++ ros_basics/ros_basics/activity_node1.py
@@ -2,19 +2,32 @@
 import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import Int64
+from rcl_interfaces.msg import SetParametersResult
 
 class NumberNode(Node):
     def __init__(self):
         super().__init__('number_publisher')
         self.publisher_ = self.create_publisher(Int64, 'number', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.publish_number)
-        self.num = 1
-        self.get_logger().info('number_publisher node started ...')    
+        self.declare_parameter('timer_period', 2.0)
+        self.declare_parameter('number', 1)
+        self.timer_period_ = self.get_parameter('timer_period').value
+        self.timer = self.create_timer(self.timer_period_, self.publish_number)
+        self.num_ = self.get_parameter('number').value
+        self.get_logger().info('number_publisher node started ...')  
+        
+        # parameter callback
+        self.add_on_set_parameters_callback(self.parameter_callback)
+        
+    def parameter_callback(self, params: list[rclpy.parameter.Parameter]):
+        for param in params:
+            if param.name == 'number':
+                self.num_ = param.value
+                self.get_logger().info(f'Parameter "number" updated to: {str(self.num_)}')
+        return SetParametersResult(successful=True)
 
     def publish_number(self):
         msg = Int64()
-        msg.data = self.num
+        msg.data = self.num_
         self.publisher_.publish(msg)
         self.get_logger().info(f'Publishing number: {msg.data}')
 
